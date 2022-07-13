458eaccaead1730c881e0ea2f7b9e54a14ec2439
Yinan Li <gabriel.li5566@gmail.com>
2022-07-13 23:30:23 +0300

    configure the route for get trips

---
+++ Backend/router/trip.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const trip_handler = require('../router_handler/trip')
+const expressJoi = require('@escook/express-joi')
+const { get_trips_schema } = require('../schema/trip')
+
+router.get('/trips', expressJoi(get_trips_schema), trip_handler.getTrips)
+
+module.exports = router
