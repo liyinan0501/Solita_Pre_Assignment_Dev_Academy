fd3727febf2f65ab8059d53dddf7fbafd9121d1d
Yinan Li <gabriel.li5566@gmail.com>
2022-07-17 19:24:35 +0300

    frontend: add StationList component

---
+++ frontend/src/components/Station/index.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { stationListAction } from 'store/actions'
+
+import { Select } from 'antd'
+const { Option } = Select
+
+function StationList({ value, onChange }) {
+  const dispatch = useDispatch()
+  const stationList = useSelector((state) => state.station)
+
+  useEffect(() => {
+    dispatch(stationListAction())
+  })
+
+  return (
+    <Select
+      placeholder="Select a departure station name or number"
+      value={value}
+      onChange={onChange}
+      style={{ width: 330 }}
+    >
+      {stationList.map((item) => (
+        <Option key={item.id} value={item.id}>
+          {item.nimi}
+          <span style={{ float: 'right' }}>{item.id}</span>
+        </Option>
+      ))}
+    </Select>
+  )
+}
+
+export default StationList
