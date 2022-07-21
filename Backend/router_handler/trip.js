54d8b6cfade557afe690c19994dd20a9c789a2a4
Yinan Li <gabriel.li5566@gmail.com>
2022-07-21 20:33:22 +0300

    backend: feat-completed related journey route handlers

--- Backend/router_handler/trip.js
+++ Backend/router_handler/trip.js
@@ -1,21 +1,49 @@
 const db = require('../db/index')
 
 exports.getTrips = (req, res) => {
-  let pageNumber
-  pageNumber = !req.query.page ? 1 : Number(req.query.page)
-  let startRow = (pageNumber - 1) * 100
-  const sqlStr1 = 'select count(*) as totalCount from journeys'
+  let { pageNumber, pageSize, departureStationId, departureDate, returnDate } =
+    req.query
+
+  pageNumber = !pageNumber ? 1 : Number(pageNumber)
+  pageSize = !pageSize ? 100 : Number(pageSize)
   let data = {}
-  db.query(sqlStr1, (err, results) => {
-    if (err) return res.cc(err, 507)
-    data.totalCount = results[0].totalCount
-    const sqlStr2 = `select * from journeys limit ${startRow}, 100`
-    db.query(sqlStr2, (err, results) => {
-      if (err) return res.cc(err, 507)
-      data.pageNumber = pageNumber
-      data.pageSize = 100
-      data.list = results
-      res.status(200).send(data)
+  let startRow = (pageNumber - 1) * pageSize
+
+  console.log(
+    pageNumber,
+    pageSize,
+    departureStationId,
+    departureDate,
+    returnDate
+  )
+
+  if (!departureStationId) {
+    const sqlStr1 = 'select count(*) as totalCount from journeys'
+    db.query(sqlStr1, (err, results) => {
+      if (err) return res.cc(err, 500)
+      data.totalCount = results[0].totalCount
+      const sqlStr2 = `select * from journeys limit ${startRow}, ${pageSize}`
+      db.query(sqlStr2, (err, results) => {
+        if (err) return res.cc(err, 500)
+        data.pageNumber = pageNumber
+        data.pageSize = pageSize
+        data.list = results
+        res.status(200).send(data)
+      })
     })
-  })
+  } else {
+    const sqlStr1 = `select count(*) as totalCount from journeys where departure_station_id = ${departureStationId} and departure >= '${departureDate}' and \`return\` <= '${returnDate}'`
+    db.query(sqlStr1, (err, results) => {
+      if (err) return res.cc(err, 500)
+      data.totalCount = results[0].totalCount
+      const sqlStr2 = `select * from journeys where departure_station_id = ${departureStationId} and departure >= '${departureDate}' and \`return\` <= '${returnDate}' order by departure asc limit ${startRow}, ${pageSize}`
+      db.query(sqlStr2, (err, results) => {
+        if (err) return res.cc(err, 500)
+        data.pageNumber = pageNumber
+        data.pageSize = pageSize
+        data.list = results
+        res.status(200).send(data)
+      })
+    })
+  }
 }
