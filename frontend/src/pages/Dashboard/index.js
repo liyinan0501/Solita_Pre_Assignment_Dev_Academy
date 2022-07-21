9338a42adc318454ad349a19cfc3b577714861c5
Yinan Li <gabriel.li5566@gmail.com>
2022-07-21 20:30:23 +0300

    frontend: update label space

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -34,31 +34,31 @@ const Dashboard = () => {
         <Row>
           <Col span={12}>
             <Form>
-              <Form.Item label="Date" name="date" labelCol={{ span: 5 }}>
+              <Form.Item label="Date" name="date" labelCol={{ span: 6 }}>
                 <RangePicker style={{ width: 266 }} />
               </Form.Item>
 
-              <Form.Item label="Time" name="Time" labelCol={{ span: 5 }}>
+              <Form.Item label="Time" name="Time" labelCol={{ span: 6 }}>
                 <TimePicker.RangePicker style={{ width: 266 }} />
               </Form.Item>
 
-              <Form.Item label="Departure Station" labelCol={{ span: 5 }}>
-                <StationList />
+              <Form.Item label="Departure Station" labelCol={{ span: 6 }}>
+                {/* <StationList /> */}
               </Form.Item>
 
-              <Form.Item label="Return Station" labelCol={{ span: 5 }}>
-                <StationList />
+              <Form.Item label="Return Station" labelCol={{ span: 6 }}>
+                {/* <StationList /> */}
               </Form.Item>
 
-              <Form.Item label="Covered Distance" labelCol={{ span: 5 }}>
+              <Form.Item label="Covered Distance" labelCol={{ span: 6 }}>
                 <InputNumber />
               </Form.Item>
 
-              <Form.Item label="Duration" labelCol={{ span: 5 }}>
+              <Form.Item label="Duration" labelCol={{ span: 6 }}>
                 <InputNumber />
               </Form.Item>
 
-              <Form.Item wrapperCol={{ offset: 5 }}>
+              <Form.Item wrapperCol={{ offset: 6 }}>
                 <Button type="primary" htmlType="submit">
                   Add Journey
                 </Button>
@@ -68,31 +68,31 @@ const Dashboard = () => {
 
           <Col span={12}>
             <Form>
-              <Form.Item label="Station Number" labelCol={{ span: 5 }}>
+              <Form.Item label="Station Number" labelCol={{ span: 6 }}>
                 <InputNumber />
               </Form.Item>
 
-              <Form.Item label="Kapasiteet" labelCol={{ span: 5 }}>
+              <Form.Item label="Kapasiteet" labelCol={{ span: 6 }}>
                 <InputNumber />
               </Form.Item>
 
-              <Form.Item label="Name" labelCol={{ span: 5 }}>
+              <Form.Item label="Name" labelCol={{ span: 6 }}>
                 <Input style={{ width: 266 }} />
               </Form.Item>
 
-              <Form.Item label="Address" labelCol={{ span: 5 }}>
+              <Form.Item label="Address" labelCol={{ span: 6 }}>
                 <Input style={{ width: 266 }} />
               </Form.Item>
 
-              <Form.Item label="City" labelCol={{ span: 5 }}>
+              <Form.Item label="City" labelCol={{ span: 6 }}>
                 <Input style={{ width: 266 }} />
               </Form.Item>
 
-              <Form.Item label="Operator" labelCol={{ span: 5 }}>
+              <Form.Item label="Operator" labelCol={{ span: 6 }}>
                 <Input style={{ width: 266 }} />
               </Form.Item>
 
-              <Form.Item wrapperCol={{ offset: 5 }}>
+              <Form.Item wrapperCol={{ offset: 6 }}>
                 <Button type="primary" htmlType="submit">
                   Add Station
                 </Button>
