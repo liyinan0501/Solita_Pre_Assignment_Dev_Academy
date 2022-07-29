193d91a1b1075e652fdd59708902b221c23acd54
Yinan Li <gabriel.li5566@gmail.com>
2022-07-29 20:59:45 +0300

    backend: feat-optimize all SQL statements

--- Backend/router_handler/station.js
+++ Backend/router_handler/station.js
@@ -6,37 +6,24 @@ exports.getStations = (req, res) => {
 
   pageNumber = !pageNumber ? 1 : Number(pageNumber)
   pageSize = !pageSize ? 100 : Number(pageSize)
-  let data = {}
+  let data = { pageNumber, pageSize }
   let startRow = (pageNumber - 1) * pageSize
 
-  console.log(pageNumber, pageSize, stationId)
   if (!stationId) {
-    const getTotal = `select count(*) as totalCount from stations`
-    db.query(getTotal, (err, results) => {
+    const getAllStations = `select *, count(*) over() as totalCount from stations order by id asc limit ${startRow}, ${pageSize}`
+    db.query(getAllStations, (err, results) => {
       if (err) return res.cc(err, 500)
-      data.totalCount = results[0].totalCount
-      const allStations = `select * from stations order by id asc limit ${startRow}, ${pageSize}`
-      db.query(allStations, (err, results) => {
-        if (err) return res.cc(err, 500)
-        data.pageNumber = pageNumber
-        data.pageSize = pageSize
-        data.list = results
-        res.status(200).send(data)
-      })
+      if (results.length < 1) return res.cc('Not found searched records', 204)
+      data = { ...data, totalCount: results[0].totalCount, list: results }
+      res.status(200).send(data)
     })
   } else {
-    const getTotalById = `select count(*) as totalCount from stations where id = ${stationId}`
-    db.query(getTotalById, (err, results) => {
+    const getStationById = `select *, count(*) over() as totalCount from stations where id = ${stationId} limit ${startRow}, ${pageSize}`
+    db.query(getStationById, (err, results) => {
       if (err) return res.cc(err, 500)
-      data.totalCount = results[0].totalCount
-      const getStationsById = `select * from stations where id = ${stationId} limit ${startRow}, ${pageSize}`
-      db.query(getStationsById, (err, results) => {
-        if (err) return res.cc(err, 500)
-        data.pageNumber = pageNumber
-        data.pageSize = pageSize
-        data.list = results
-        res.status(200).send(data)
-      })
+      if (results.length < 1) return res.cc('Not found searched records', 204)
+      data = { ...data, totalCount: results[0].totalCount, list: results }
+      res.status(200).send(data)
     })
   }
 }
@@ -52,33 +39,25 @@ exports.getStationList = (req, res) => {
 
 exports.getSingleStation = (req, res) => {
   let data = {}
-
   const { stationId } = req.params
-  const sqlStr = `select * from stations where id = ${stationId}`
-  const sqlStr1 = `select count(*) as startCount from journeys where departure_station_id = ${stationId}`
-  const sqlStr2 = `select count(*) as endCount from journeys where return_station_id = ${stationId}`
-  const sqlStr3 = `select AVG(covered_distance) as startAvg from journeys where departure_station_id = ${stationId}`
-  const sqlStr4 = `select AVG(covered_distance) as endAvg from journeys where return_station_id = ${stationId}`
-  const sqlStr5 = `select return_station_name, count(1) as times from journeys where departure_station_id = ${stationId} group by return_station_name order by times desc limit 5`
-  const sqlStr6 = `select departure_station_name, count(1) as times from journeys where return_station_id = ${stationId}  group by departure_station_name order by times desc limit 5`
 
-  // const sqlStr = `select * from stations where id = ${stationId}`
-  // const sqlStr1 = `select count(*) as startCount, AVG(covered_distance) as startAvg from journeys where departure_station_id = ${stationId}`
-  // const sqlStr2 = `select count(*) as endCount, select AVG(covered_distance) as endAvg from journeys where return_station_id = ${stationId}`
-  // const sqlStr5 = `select return_station_name, count(1) as times from journeys where departure_station_id = ${stationId} group by return_station_name order by times desc limit 5`
-  // const sqlStr6 = `select departure_station_name, count(1) as times from journeys where return_station_id = ${stationId}  group by departure_station_name order by times desc limit 5`
+  const getStationById = `select * from stations where id = ${stationId}`
+  const getStartCountAndAvg = `select count(*) as startCount, AVG(covered_distance) as startAvg from journeys where departure_station_id = ${stationId}`
+  const getEndCountAndAvg = `select count(*) as endCount, AVG(covered_distance) as endAvg from journeys where return_station_id = ${stationId}`
+  const getReturnTop5 = `select return_station_name, count(1) as times from journeys where departure_station_id = ${stationId} group by return_station_name order by times desc limit 5`
+  const getDepartureTop5 = `select departure_station_name, count(1) as times from journeys where return_station_id = ${stationId}  group by departure_station_name order by times desc limit 5`
 
   db.query(
-    `${sqlStr};${sqlStr1};${sqlStr2};${sqlStr3};${sqlStr4};${sqlStr5};${sqlStr6}`,
+    `${getStationById};${getStartCountAndAvg};${getEndCountAndAvg};${getReturnTop5};${getDepartureTop5}`,
     function (err, results, fields) {
       if (err) return res.cc(err, 500)
-      data = results[0][0]
-      data.startCount = results[1][0].startCount
-      data.endCount = results[2][0].endCount
-      data.startAvg = results[3][0].startAvg
-      data.endAvg = results[4][0].endAvg
-      data.topReturn = results[5]
-      data.topDeparture = results[6]
+      data = {
+        ...results[0][0],
+        ...results[1][0],
+        ...results[2][0],
+        topReturn: results[3],
+        topDeparture: results[4],
+      }
       res.status(200).send(data)
     }
   )
--- Backend/router_handler/trip.js
+++ Backend/router_handler/trip.js
@@ -7,57 +7,36 @@ exports.getTrips = (req, res) => {
 
   pageNumber = !pageNumber ? 1 : Number(pageNumber)
   pageSize = !pageSize ? 100 : Number(pageSize)
-  let data = {}
+  let data = { pageNumber, pageSize }
   let startRow = (pageNumber - 1) * pageSize
 
-  console.log(
-    pageNumber,
-    pageSize,
-    departureStationId,
-    departureDate,
-    returnDate
-  )
-
   if (!departureStationId) {
-    const getTotal = 'select count(*) as totalCount from journeys'
-    db.query(getTotal, (err, results) => {
+    const getAllTrips = `select *, count(*) over() as totalCount from journeys limit ${startRow}, ${pageSize}`
+    db.query(getAllTrips, (err, results) => {
       if (err) return res.cc(err, 500)
-      data.totalCount = results[0].totalCount
-      const getJourneys = `select * from journeys limit ${startRow}, ${pageSize}`
-      db.query(getJourneys, (err, results) => {
-        if (err) return res.cc(err, 500)
-        data.pageNumber = pageNumber
-        data.pageSize = pageSize
-        data.list = results
-        res.status(200).send(data)
-      })
+      if (results.length < 1) return res.cc('Not found searched records', 204)
+      data = { ...data, totalCount: results[0].totalCount, list: results }
+      res.status(200).send(data)
     })
   } else {
-    const getTotal = `select count(*) as totalCount from journeys where departure_station_id = ${departureStationId} and departure >= '${departureDate}' and \`return\` <= '${returnDate}'`
-    db.query(getTotal, (err, results) => {
+    const searchTrips = `select *, count(*) over() as totalCount from journeys where departure_station_id = ${departureStationId} and departure >= '${departureDate}' and \`return\` <= '${returnDate}' order by departure asc limit ${startRow}, ${pageSize}`
+    db.query(searchTrips, (err, results) => {
       if (err) return res.cc(err, 500)
-      data.totalCount = results[0].totalCount
-      const getJourneysByIdDate = `select * from journeys where departure_station_id = ${departureStationId} and departure >= '${departureDate}' and \`return\` <= '${returnDate}' order by departure asc limit ${startRow}, ${pageSize}`
-      db.query(getJourneysByIdDate, (err, results) => {
-        if (err) return res.cc(err, 500)
-        data.pageNumber = pageNumber
-        data.pageSize = pageSize
-        data.list = results
-        res.status(200).send(data)
-      })
+      if (results.length < 1) return res.cc('Not found searched records', 204)
+      data = { ...data, totalCount: results[0].totalCount, list: results }
+      res.status(200).send(data)
     })
   }
 }
 
 // add a trip router handler
 exports.addTrip = (req, res) => {
-  console.log(req.body)
-  // const insertTrip = `insert journeys set ?, departure_station_name = (select nimi from stations where id = ${req.body.departure_station_id}), return_station_name = (select nimi from stations where id = ${req.body.return_station_id})`
-  // const data = { ...req.body }
-  // console.log(data)
-  // db.query(insertTrip, { ...req.body }, (err, results) => {
-  //   if (err) return res.cc(err)
-  //   if (results.affectedRows !== 1) return res.cc('Adding fails', 507)
-  //   return res.cc('adding journey succeeds!', 201)
-  // })
+  const insertTrip = `insert journeys set ?, departure_station_name = (select nimi from stations where id = ${req.body.departure_station_id}), return_station_name = (select nimi from stations where id = ${req.body.return_station_id})`
+  const data = { ...req.body }
+
+  db.query(insertTrip, { ...req.body }, (err, results) => {
+    if (err) return res.cc(err)
+    if (results.affectedRows !== 1) return res.cc('Adding fails', 507)
+    return res.cc('adding journey succeeds!', 201)
+  })
 }
