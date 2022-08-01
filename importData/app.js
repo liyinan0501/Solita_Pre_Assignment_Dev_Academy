83a6d8a2eb6f644555ff5c45185f4c54b8b07a66
Yinan Li <gabriel.li5566@gmail.com>
2022-08-01 20:31:18 +0300

    backend: update user guideness for import data

--- importData/app.js
+++ importData/app.js
@@ -9,6 +9,8 @@ db.query('select 1', (err, results) => {
 
 // Importing the trip and station
 const dataFiles = ['2021-05.csv']
+// const dataFiles = ['2021-05.csv', '2021-06.csv', '2021-07.csv']
+
 const importHandler = require('./import_handler/index')
 
 const readline = require('readline')
@@ -17,25 +19,24 @@ let r1 = readline.createInterface({
   output: process.stdout,
 })
 
-r1.setPrompt(
-  'Do you want to import all journeys and stations to database? (yes/no)> '
-)
-r1.prompt()
-r1.on('line', (line) => {
-  switch (line.trim()) {
-    case 'yes':
-      console.log('Starting...')
-      importHandler.importStation()
-      importHandler.importTrip(dataFiles)
-      break
-    case 'no':
-      break
-    default:
-      console.log('Incorrect answer, closing.')
-      r1.close
-      break
+r1.question(
+  `Do you want to import all journeys and stations to database? (yes/no) `,
+  (answer) => {
+    switch (answer.trim()) {
+      case 'yes':
+        console.log('Starting...')
+        importHandler.importStation()
+        importHandler.importTrip(dataFiles)
+        break
+      case 'no':
+        break
+      default:
+        console.log('Incorrect answer, closing.')
+        r1.close
+        break
+    }
   }
-})
+)
 
 r1.on('close', function () {
   process.exit(0)
