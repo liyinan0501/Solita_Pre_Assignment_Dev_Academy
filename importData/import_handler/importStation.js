742604793dbe508cab66a588a48a2d5d70592099
Yinan Li <gabriel.li5566@gmail.com>
2022-08-01 14:13:46 +0300

    backend: optimize importStation

--- importData/import_handler/importStation.js
+++ importData/import_handler/importStation.js
@@ -1,5 +1,6 @@
 const db = require('../db/index')
 const dbInfo = require('../db/dbInfo')
+const path = require('path')
 
 module.exports = importStation = () => {
   // Creating the table attributes
@@ -41,43 +42,71 @@ module.exports = importStation = () => {
     { timestamps: false }
   )
 
-  sequelize.sync({ alter: true }).then(() => {
-    console.log('Table is created')
-    // Importing data from CSV file
-    const csv = require('csv-parser')
-    const fs = require('fs')
-    const sqlStr = `insert into ${dbInfo.tableStation} set ?`
-    fs.createReadStream('./asset/station.csv')
-      .pipe(
-        csv({
-          skipLines: 1,
-          headers: [
-            'fid',
-            'id',
-            'nimi',
-            'namn',
-            'name',
-            'osoite',
-            'adress',
-            'kaupunki',
-            'stad',
-            'operaattor',
-            'kapasiteet',
-            'x',
-            'y',
-          ],
-        })
-      )
-      .on('data', (data) => {
-        // Inserting valid records to database
-        db.query(sqlStr, data, (err, results) => {
-          if (err) {
-            return console.log(err)
-          }
-          if (results.affectedRows !== 1) {
-            return console.log('import failed')
+  let dataArray = []
+
+  sequelize.sync({ alter: true }).then(async () => {
+    try {
+      dataArray = await readStations()
+      console.log(`Reading Station file completed, start to insert...`)
+      let totalRows = await importStations()
+      console.log(`All stations import completed, imported ${totalRows} rows.`)
+      dataArray = []
+    } catch (e) {
+      console.log(e)
+    }
+  })
+
+  function importStations() {
+    console.log('Inserting...')
+    const insertStation = `insert into ${dbInfo.tableStation} set ?`
+    let counter = 0
+    return new Promise((resolve) => {
+      dataArray.forEach((item) => {
+        db.query(insertStation, item, (err, results) => {
+          if (err) return console.log('Insert error:', err)
+          if (results.affectedRows === 1) counter++
+          if (counter === dataArray.length) {
+            resolve(counter)
           }
         })
       })
-  })
+    })
+  }
+
+  function readStations() {
+    return new Promise((resolve) => {
+      const csv = require('csv-parser')
+      const fs = require('fs')
+      fs.createReadStream(path.join(__dirname, './asset/station.csv'))
+        .pipe(
+          csv({
+            skipLines: 1,
+            headers: [
+              'fid',
+              'id',
+              'nimi',
+              'namn',
+              'name',
+              'osoite',
+              'adress',
+              'kaupunki',
+              'stad',
+              'operaattor',
+              'kapasiteet',
+              'x',
+              'y',
+            ],
+          })
+        )
+        .on('data', (data) => {
+          dataArray.push(data)
+        })
+        .on('error', (error) => {
+          console.log('ReadStream error:', error.message)
+        })
+        .on('end', function () {
+          resolve(dataArray)
+        })
+    })
+  }
 }
